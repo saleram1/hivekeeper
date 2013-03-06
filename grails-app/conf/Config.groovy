c6f4bf7242f44def6d1a20f645c07340f3eb058b
Michael J Salera <mikesalera@mac.com>
2013-03-05 21:15:24 -0800

    Added Spring Security core

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -89,3 +89,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'hivekeeper.SecUser'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'hivekeeper.SecUserSecRole'
+grails.plugins.springsecurity.authority.className = 'hivekeeper.SecRole'
